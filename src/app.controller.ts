4eb2266fb95b43bb5be9a4e7b7405d2ae7c1c344
Pedro Sousa <pedrohigor.dev@gmail.com>
2024-01-22 13:23:57 -0300

    Configurando prisma ...

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller('/api/v1/url-shortener-teddy/')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
   @Get('/hello')
-  getHello(): string {
-    return this.appService.getHello()
+  async getHello() {
+    return await this.prisma.user.findMany()
   }
 }
