169b7e998768e4df7fc1cbbc38cc02b083b31275
Pedro Sousa <pedrohigor.dev@gmail.com>
2024-01-22 21:11:41 -0300

    Implementando delete de URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { UrlShortenerController } from './controllers/url-shortener.controller'
 import { EditUrlController } from './controllers/edit-original-url.controller'
+import { DeleteUrlController } from './controllers/delete-url-shortened.controller'
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { EditUrlController } from './controllers/edit-original-url.controller'
     AuthenticateController,
     UrlShortenerController,
     EditUrlController,
+    DeleteUrlController,
   ],
   providers: [PrismaService],
 })
---
+++ src/controllers/delete-url-shortened.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Delete,
+  HttpCode,
+  NotFoundException,
+  Query,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+import { ZoodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+
+const deleteUrlQuerySchema = z.object({
+  urlId: z.string(),
+})
+
+type DeleteUrlQuerySchema = z.infer<typeof deleteUrlQuerySchema>
+
+@Controller('/shorten')
+@UseGuards(JwtAuthGuard)
+@UsePipes(new ZoodValidationPipe(deleteUrlQuerySchema))
+export class DeleteUrlController {
+  constructor(private prisma: PrismaService) {}
+
+  @Delete()
+  @HttpCode(201)
+  @UsePipes(new ZoodValidationPipe(deleteUrlQuerySchema))
+  async handle(@Query() query: DeleteUrlQuerySchema) {
+    const { urlId } = query
+
+    const url = await this.prisma.url.findFirst({
+      where: {
+        id: urlId,
+      },
+    })
+
+    if (!url) {
+      throw new NotFoundException('URl not exists')
+    }
+
+    await this.prisma.url.delete({
+      where: {
+        id: urlId,
+      },
+    })
+  }
+}
