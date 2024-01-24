85c36cf4ea6fb4da6c336bc1ebc3e189ecf553c6
Pedro Sousa <pedrohigor.dev@gmail.com>
2024-01-24 00:22:26 -0300

    Removendo código inúteis

--- src/infra/http/controllers/url-shortener.controller.ts
+++ src/infra/http/controllers/url-shortener.controller.ts
@@ -1,10 +1,7 @@
 import { CurrentUser } from '@/infra/auth/current-user-decorator'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { UserPayload } from '@/infra/auth/jwt.strategy'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
-import { Env } from '@/infra/env/env'
 import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import { Public } from '@/infra/auth/public'
 import { RegisterUrlUseCase } from '@/domain/user/application/use-cases/register-urls'
 
@@ -15,11 +12,7 @@ interface OriginalUrl {
 @Controller('/shorten')
 @UseGuards(JwtAuthGuard)
 export class UrlShortenerController {
-  constructor(
-    private prisma: PrismaService,
-    private config: ConfigService<Env, true>,
-    private registerUrl: RegisterUrlUseCase,
-  ) {}
+  constructor(private registerUrl: RegisterUrlUseCase) {}
 
   @Post()
   @HttpCode(201)
